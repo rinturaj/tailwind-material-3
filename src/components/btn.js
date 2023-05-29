c9aed3f7e175921b12f631edc134b1b8ac3f6945
Rintu Raj.c <rinturajc180000@gmail.com>
2023-05-29 12:41:56 +0530

    filled and filled toned button finised

--- src/components/btn.js
+++ src/components/btn.js
@@ -2,7 +2,7 @@ const { map } = require("lodash");
 const plugin = require("tailwindcss/plugin");
 
 module.exports = plugin(function ({ addComponents, theme, config }) {
-  
+ 
   const color = Object.keys(config("theme.colors")).filter((x) =>
     /^\w+-light/.test(x)
   );
@@ -80,56 +80,88 @@ module.exports = plugin(function ({ addComponents, theme, config }) {
     },
 
     ".filled-btn": {
-      backgroundColor: theme("colors.primary.800"),
-      color: theme("colors.primary.50"),
+      backgroundColor: theme("colors.primary-light"),
+      color: theme("colors.on-primary-light"),
       borderRadius: theme("borderRadius.extralarge"),
       paddingBottom: theme("spacing.small"),
       paddingRight: theme("spacing.large"),
       paddingTop: theme("spacing.extrasmall"),
       paddingLeft: theme("spacing.large"),
       "&:disabled,filled-btn[disabled]": {
-        backgroundColor: theme("colors.primary.100") + "!important",
-        color: theme("colors.primary.400") + "!important",
+        backgroundColor: theme("colors.primary-light"),
+        color: theme("colors.on-primary-light"),
+        opacity:'.12',
         boxShadow: "none !important",
+        borderRadius: 'none !important'
       },
       "&:hover": {
-        backgroundColor: theme("colors.primary.600"),
-        color: theme("colors.primary.50"),
+        backgroundColor: theme("colors.primary-light-hover"),
+        color: theme("colors.on-primary-light"),
         boxShadow: theme("boxShadow.DEFAULT"),
       },
       "&:active": {
-        backgroundColor: theme("colors.primary.400"),
-        color: theme("colors.primary.50"),
+        backgroundColor: theme("colors.primary-light-press"),
+        color: theme("colors.on-primary-light-press"),
         boxShadow: "none",
         borderRadius: theme("borderRadius.medium"),
       },
     },
     ".filled-tonal-btn": {
-      backgroundColor: theme("colors.primary.100"),
-      color: theme("colors.secondary.900"),
+      backgroundColor: theme("colors.primary-container-light"),
+      color: theme("colors.on-primary-container-light"),
       borderRadius: theme("borderRadius.extralarge"),
       paddingBottom: theme("spacing.small"),
       paddingRight: theme("spacing.large"),
       paddingTop: theme("spacing.extrasmall"),
       paddingLeft: theme("spacing.large"),
       "&:disabled,filled-btn[disabled]": {
-        backgroundColor: theme("colors.primary.100") + "!important",
-        color: theme("colors.primary.400") + "!important",
+        backgroundColor: theme("colors.primary-container-light") + "!important",
+        color: theme("colors.primary-on-container-light") + "!important",
+        opacity:'.12',
         boxShadow: "none !important",
+        borderRadius: 'none !important'
       },
       "&:hover": {
-        backgroundColor: theme("colors.primary.200"),
-        color: theme("colors.primary.900"),
+        backgroundColor: theme("colors.primary-container-light-hover"),
+        color: theme("colors.on-primary-container-light"),
         boxShadow: theme("boxShadow.DEFAULT"),
       },
       "&:active": {
-        backgroundColor: theme("colors.primary.300"),
-        color: theme("colors.primary.900"),
+        backgroundColor: theme("colors.primary-container-light-press"),
+        color: theme("colors.on-primary-container-light"),
         boxShadow: "none",
         borderRadius: theme("borderRadius.medium"),
       },
     },
-    ".outlined-btn": {},
+    ".outlined-btn": {
+      backgroundColor: 'transparent',
+      color: theme("colors.primary-light"),
+      borderRadius: theme("borderRadius.extralarge"),
+      paddingBottom: theme("spacing.small"),
+      paddingRight: theme("spacing.large"),
+      paddingTop: theme("spacing.extrasmall"),
+      paddingLeft: theme("spacing.large"),
+      borderColor: theme("colors.outline"),
+      borderWidth: '1px',
+      "&:disabled,filled-btn[disabled]": {
+        backgroundColor: theme("colors.inverse-primary") + "!important",
+        color: theme("colors.primary") + "!important",
+        opacity:'.12',
+        boxShadow: "none !important",
+        borderRadius: 'none !important'
+      },
+      "&:hover": {
+        backgroundColor: theme("colors.inverse-primary"),
+        color: theme("colors.on-primary-container-light"),
+        boxShadow: theme("boxShadow.DEFAULT"),
+      },
+      "&:active": {
+        backgroundColor: theme("colors.primary-container-light-press"),
+        color: theme("colors.on-primary-container-light"),
+        boxShadow: "none",
+        borderRadius: theme("borderRadius.medium"),
+      },
+    },
     ".text-btn": {},
     ".icon-btn": {},
     ".fab-btn": {},
